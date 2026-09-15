c76c9dd980a62b58bdac30a3d37869291f2b55cc
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-15 10:13:06 +0100

    create Paiement class and its setters, getters and constructor

--- src/main/java/com/abonnement/entity/Paiement.java
+++ src/main/java/com/abonnement/entity/Paiement.java
@@ -1,4 +1,70 @@
 package com.abonnement.entity;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Paiement {
+    private String idPaiement;
+    private String idAbonnement;
+    private LocalDate dateEcheance;
+    private LocalDate datePaiement;
+    private TypePaiement typePaiement;
+    private StatutPaiement statut;
+
+    public Paiement(String idAbonnement, LocalDate dateEcheance, LocalDate datePaiement, TypePaiement typePaiement, StatutPaiement statut) {
+        this.idPaiement = UUID.randomUUID().toString();
+        this.idAbonnement = idAbonnement;
+        this.dateEcheance = dateEcheance;
+        this.datePaiement = datePaiement;
+        this.typePaiement = typePaiement;
+        this.statut = statut;
+    }
+
+    public String getIdPaiement() {
+        return idPaiement;
+    }
+
+    public void setIdPaiement(String idPaiement) {
+        this.idPaiement = idPaiement;
+    }
+
+    public String getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public void setIdAbonnement(String idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    public LocalDate getDateEcheance() {
+        return dateEcheance;
+    }
+
+    public void setDateEcheance(LocalDate dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public LocalDate getDatePaiement() {
+        return datePaiement;
+    }
+
+    public void setDatePaiement(LocalDate datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public TypePaiement getTypePaiement() {
+        return typePaiement;
+    }
+
+    public void setTypePaiement(TypePaiement typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public StatutPaiement getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutPaiement statut) {
+        this.statut = statut;
+    }
 }
