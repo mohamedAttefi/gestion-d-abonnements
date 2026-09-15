9972f353371a58e3349e9fce1405ced0c0e9c318
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-15 17:22:03 +0100

    add gitignore

--- src/main/java/com/abonnement/Main.java
+++ src/main/java/com/abonnement/Main.java
@@ -1,4 +1,15 @@
 package com.abonnement;
 
+import com.abonnement.databaseSetup.DatabaseSetup;
+
 public class Main {
+
+    public static void main(String[] args){
+        try{
+            DatabaseSetup.getConnection();
+            System.out.println("connection established");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
