df19a458818d3ddd4364f8772801a6ec96f49478
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-15 10:12:21 +0100

    create child class of abonnement, AbonnementAvecEngagement and its setters, getters and constructor

--- src/main/java/com/abonnement/entity/AbonnementAvecEngagement.java
+++ src/main/java/com/abonnement/entity/AbonnementAvecEngagement.java
@@ -1,4 +1,21 @@
 package com.abonnement.entity;
 
-public class AbonnementAvecEngagement {
+import java.time.LocalDate;
+
+public class AbonnementAvecEngagement extends Abonnement {
+
+    private int dureeEngagementMois;
+
+    public AbonnementAvecEngagement(String nomService, int montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatutAbonnement statut, int dureeEngagementMois) {
+        super(nomService, montantMensuel, dateDebut, dateFin, statut);
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
+
+    public int getDureeEngagementMois() {
+        return dureeEngagementMois;
+    }
+
+    public void setDureeEngagementMois(int dureeEngagementMois) {
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
 }
