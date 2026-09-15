488f590954e1375b9beed7b6d139c6bb5843a42f
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-15 10:10:58 +0100

    create abstract class abonnement and its setters, getters and constructor

--- src/main/java/com/abonnement/entity/Abonnement.java
+++ src/main/java/com/abonnement/entity/Abonnement.java
@@ -1,4 +1,67 @@
 package com.abonnement.entity;
 
-public class Abonnement {
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Abonnement {
+    protected String id;
+    protected String nomService;
+    protected int montantMensuel;
+    protected LocalDate dateDebut;
+    protected LocalDate dateFin;
+    protected StatutAbonnement statut;
+
+
+    public Abonnement(String nomService, int montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatutAbonnement statut){
+        this.id = UUID.randomUUID().toString();
+        this.nomService = nomService;
+        this.montantMensuel = montantMensuel;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    public StatutAbonnement getStatut() {
+        return statut;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public void setMontantMensuel(int montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public void setStatut(StatutAbonnement statut) {
+        this.statut = statut;
+    }
 }
