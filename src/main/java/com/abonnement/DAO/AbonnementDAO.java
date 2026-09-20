2999a569ef3bbfe0af48d13d51bf84d2825223b0
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-20 14:25:40 +0000

    implement abonnement dao interface

--- src/main/java/com/abonnement/DAO/AbonnementDAO.java
+++ src/main/java/com/abonnement/DAO/AbonnementDAO.java
@@ -1,4 +1,18 @@
 package com.abonnement.DAO;
 
-public class AbonnementDAO {
+import com.abonnement.entity.Abonnement;
+
+import java.util.List;
+
+public interface AbonnementDAO {
+
+    void ajouter(Abonnement abonnement);
+
+    List<Abonnement> findAll();
+
+    Abonnement findById(String id);
+
+    void modifier(Abonnement abonnement);
+
+    void supprimer(String id);
 }
